71099a28ca0ca8e8502508cf19a9406c33734fd0
drplague1 <diebarian1203@yahoo.com>
2022-11-30 21:26:42 -0500

    hello

---
+++ Java/Game/src/Paddle.java
@@ -0,0 +1,31 @@
+import javax.imageio.ImageIO;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.io.File;
+import java.io.IOException;
+
+public class Paddle {
+    public File Paddle = new File("C:/Users/kali/Downloads/Java/Game/src/Paddle.png");
+    public Image playerImage;
+    public Rectangle paddle;
+    public int y;
+    public int x;
+    //
+    public Paddle(int x, int y) {
+        this.x = x;
+        this.y = y;
+        paddle = new Rectangle(x, y, 25, 140);
+    }
+    // draw method for createing the players paddle
+    public void draw(Graphics g) {
+        // Loads Paddle asset
+        try {
+            playerImage = ImageIO.read(Paddle);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        // graphics for the players paddle, this gets used by paint method later to paint it to the screen
+        g.drawImage(playerImage, x, y, 25, 140, null);
+    }
+}
